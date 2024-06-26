8e11ad5f04bc8228e6d753ba0a0a47c0d0ca6787
PortiESP <portiesp@gmail.com>
2024-06-26 13:58:09 +0200

    Close overlay

--- src/components/tool-overlay/ToolOverlay.jsx
+++ src/components/tool-overlay/ToolOverlay.jsx
@@ -1,5 +1,8 @@
-import GraphEditor from "./overlay-nodes/OverlayLiveEditor"
+import LiveEditor from "./overlay-nodes/OverlayLiveEditor"
+import CloseIcon from "../../assets/close"
+import ArrowLeft from "../../assets/arrow-left"
 import scss from "./toolsOverlay.module.scss"
+import { useState } from "react"
 
 export default function ToolOverlay(props) {
 
@@ -8,11 +11,28 @@ export default function ToolOverlay(props) {
         <div className={scss.canvas_wrap}>
           {props.children}
         </div>
-        <div className={[scss.overlay, scss.overlay_left].join(" ")}>
-            <GraphEditor />
-        </div>
+        <Overlay className={scss.overlay_left}>
+          <LiveEditor />
+        </Overlay>
         {/* <div className={[scss.overlay, scss.overlay_right].join(" ")}>
         </div> */}
       </div>
+  )
+}
+
+
+function Overlay(props){
+  const [closed, setClosed] = useState(false)
+
+  const closedClass = props.closedRight ? scss.closed_right : scss.closed_left
+
+  return (
+    <div className={[scss.overlay, props.className, closed ? closedClass: ""].join(" ")}>
+        {props.children}
+        <div className={scss.toggle} onClick={() => setClosed(old => !old)}>
+          {!closed ? <CloseIcon /> : <ArrowLeft />}
+        </div>
+    </div>
+
   )
 }
