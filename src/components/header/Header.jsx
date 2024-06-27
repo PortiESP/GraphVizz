834b25aef53e41ab2f21cca1370889411e410cc7
PortiESP <portiesp@gmail.com>
2024-06-27 07:29:14 +0200

    Zoom label header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,28 +1,36 @@
 import scss from "./header.module.scss"
-{/* Import svg */}
+{/* Import svg */ }
 import ListIcon from "../../assets/list"
+import { useState } from "react"
+import { useEffect } from "react"
 
 export default function Header(props) {
 
-  return (
-      <header className={scss.header_wrap}>
-        <nav>
-            <div className={scss.menu_icon}>
-                <ListIcon />
-            </div>
-            <div className={scss.menu_options}>
-                <ul>
-                    <li>Load graph</li>
-                    <li>Algorithms</li>
-                    <li>Settings</li>
-                    <li>Help</li>
-                </ul>
-            </div>
-            <div className={scss.menu_info}>
-                <button className={scss.share}>Share</button>
-                <span className={scss.zoom}>200%</span>
-            </div>
-        </nav>
-      </header>
-  )
+    const [zoom, setZoom] = useState(window.cvs?.zoom)
+
+    useEffect(() => {
+        window.setZoom = setZoom
+    }, [])
+
+    return (
+        <header className={scss.header_wrap}>
+            <nav>
+                <div className={scss.menu_icon}>
+                    <ListIcon />
+                </div>
+                <div className={scss.menu_options}>
+                    <ul>
+                        <li>Load graph</li>
+                        <li>Algorithms</li>
+                        <li>Settings</li>
+                        <li>Help</li>
+                    </ul>
+                </div>
+                <div className={scss.menu_info}>
+                    <button className={scss.share}>Share</button>
+                    <span className={scss.zoom}>{Math.round(zoom*100)}%</span>
+                </div>
+            </nav>
+        </header>
+    )
 }
