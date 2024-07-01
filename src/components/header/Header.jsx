be3fb344b3567cdc5be8cd4f8c55b18e2a6b0bbc
PortiESP <portiesp@gmail.com>
2024-07-01 10:30:49 +0200

    Updated header to hide options while not in graph

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,6 +8,7 @@ import SharePopup from "./share-popup/SharePopup"
 import HamburgerMenu from "./hamburger-menu/Hamburger"
 import { resetZoom } from "../graph-manager/canvas-component/utils/zoom"
 import { focusOnAllNodes } from "../graph-manager/utils/view"
+import { useLocation } from "react-router-dom"
 const HomeIcon = () => <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.77778 10.2222V18C5.77778 19.1046 6.67321 20 7.77778 20H12M5.77778 10.2222L11.2929 4.70711C11.6834 4.31658 12.3166 4.31658 12.7071 4.70711L17.5 9.5M5.77778 10.2222L4 12M18.2222 10.2222V18C18.2222 19.1046 17.3268 20 16.2222 20H12M18.2222 10.2222L20 12M18.2222 10.2222L17.5 9.5M17.5 9.5V6M12 20V15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/></svg>
 
 export default function Header(props) {
@@ -15,6 +16,12 @@ export default function Header(props) {
     const [zoom, setZoom] = useState(window.cvs?.zoom)
     const [showSharePopup, setShowSharePopup] = useState(false)
     const [showMenu, setShowMenu] = useState(false)
+    const location = useLocation()
+    const [isGraphPage, setIsGraphPage] = useState(location.pathname === "/")
+
+    useEffect(() => {
+        setIsGraphPage(location.pathname === "/")
+    }, [location])
 
     useEffect(() => {
         window.setZoom = setZoom
@@ -32,9 +39,12 @@ export default function Header(props) {
         <header className={scss.header_wrap}>
             <nav>
                 <div className={scss.menu_logo}>
-                    <div className={scss.list_icon} onClick={() => setShowMenu(old => !old)}>
-                        <HomeIcon />
-                    </div>
+                    {
+                        isGraphPage && 
+                        <div className={scss.list_icon} onClick={() => setShowMenu(old => !old)}>
+                            <HomeIcon />
+                        </div>
+                    }
                     <Link to="/">
                         <div className={scss.logo}>
                             <Logo />
@@ -50,8 +60,13 @@ export default function Header(props) {
                     </ul>
                 </div>
                 <div className={scss.menu_info}>
-                    <button className={scss.share} onClick={handleShare}>Share</button>
-                    <span className={scss.zoom} onClick={handleResetZoom}>{Math.round(zoom*100)}%</span>
+                    {
+                        isGraphPage && 
+                        <>
+                        <button className={scss.share} onClick={handleShare}>Share</button>
+                        <span className={scss.zoom} onClick={handleResetZoom}>{Math.round(zoom*100)}%</span>
+                        </>
+                    }
                 </div>
             </nav>
         </header>
