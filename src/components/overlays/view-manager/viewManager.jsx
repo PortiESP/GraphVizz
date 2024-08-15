6603f8b305b24e77f0f4a67ee56789e665041811
Porti <admin@0xPorti.com>
2024-08-15 11:01:46 +0200

    fixed drag viewManager

--- src/components/overlays/view-manager/viewManager.jsx
+++ src/components/overlays/view-manager/viewManager.jsx
@@ -7,6 +7,7 @@ import CloseIcon from "@assets/close.svg?react"
 import DashIcon from "@assets/dash.svg?react"
 import DotsIcon from "@assets/6-dots.svg?react"
 import toast from "react-hot-toast"
+import { useLayoutEffect } from "react"
 
 // DEBUG
 const DEFAULT_VIEW = {
@@ -16,14 +17,26 @@ const DEFAULT_VIEW = {
     // type: "info",
     // info: "This is an example info view"
 
-    type: "1-select",
-    label: "Select a node",
-    options: ["A", "B", "C", "D"],
-    placeholderOption: true,
+    // type: "1-select",
+    // label: "Select a node",
+    // options: ["A", "B", "C", "D"],
+    // // Run when an option is selected
+    // setup: () => <table><thead><tr><th>Data</th></tr></thead><tr><td>Setup</td></tr><tr><td>Setup</td></tr><tr><td>Setup</td></tr><tr><td>Setup</td></tr></table>,
+    // // Run when an option is selected
+    // onChange: selected => console.log("selected", selected) || <p data-widget-type="code">{selected}</p>,  // Run when an option is selected
+
+    type: "2-select",
+    labelA: "Select node A",
+    labelB: "Select node B",
+    labelAB: "to",
+    optionsA: ["A1", "A2", "A3", "A4"],
+    optionsB: ["B1", "B2", "B3", "B4"],
     // Run when an option is selected
-    setup: () => <table><thead><tr><th>Data</th></tr></thead><tr><td>Setup</td></tr><tr><td>Setup</td></tr><tr><td>Setup</td></tr><tr><td>Setup</td></tr></table>,
-    // Run when an option is selected
-    onChange: selected => console.log("selected", selected) || <p data-widget-type="code">{selected}</p>  // Run when an option is selected
+    setup: () => <table><thead><tr><th>Data</th></tr></thead><tbody>
+        <tr><td>Setup</td></tr><tr><td>Setup</td></tr><tr><td>Setup</td></tr><tr><td>Setup</td></tr>
+    </tbody></table>,
+    // Run when an option is selected in any of the selects
+    onChange: (selected, select) => console.log("selected", selected, select) || <p data-widget-type="code">{selected}</p>,  // Run when an option is selected
 
 }
 
@@ -54,7 +67,7 @@ export default function ViewManager(props) {
         window.ui.set("setLastResult", result => setLastResult(result))
     }, [])
 
-    function addPos(x, y) {
+    const addPos = (x, y) => {
         setPos(old => {
             const newX = old.x + x
             const newY = old.y + y
@@ -63,18 +76,22 @@ export default function ViewManager(props) {
             return { x: newX, y: newY }
         })
     }
-    function handleMouseMove(e) {
-        if (allowDrag) addPos(e.movementX, e.movementY)
+    const handleMouseMove = (e) => {
+        console.log("moving ", allowDrag)
+        if (allowDrag) {
+            console.log("moving")
+            addPos(e.movementX, e.movementY)
+        }
     }
-    function handleMouseDown(e) {
+    const handleMouseDown = (e) => {
         setAllowDrag(true)
-        console.log("mousedown", e)
+        console.log("!!! allowed")
     }
-    function handleMouseUp(e) {
+    const handleMouseUp = (e) => {
         setAllowDrag(false)
-        console.log("mouseup", e)
+        console.log("!!! disabled")
     }
-    function handleCopy() {
+    const handleCopy = () => {
         if (!lastResult) {
             toast.error("No result to copy")
             return
@@ -84,9 +101,17 @@ export default function ViewManager(props) {
         toast.success("Copied to clipboard")
     }
 
+    useLayoutEffect(() => {
+        window.addEventListener("mousemove", handleMouseMove)
+        window.addEventListener("mouseup", handleMouseUp)
+        return () => {
+            window.removeEventListener("mousemove", handleMouseMove)
+            window.removeEventListener("mouseup", handleMouseUp)
+        }
+    }, [allowDrag])
 
     return (show ?
-        <div className={scss.wrap} onMouseUp={handleMouseUp} onMouseMove={handleMouseMove}>
+        <div className={scss.wrap} >
             <div className={scss.view_wrap} ref={$container} >
                 <div className={scss.header} onMouseDown={handleMouseDown}>
                     <div className={scss.view_title}>
@@ -113,8 +138,8 @@ export default function ViewManager(props) {
                             {
                                 // Show the correct view based on the type
                                 data.type === "info" ? <Info data={data} setOutput={setOutput} />
-                                : data.type === "1-select" ? <SelectNode data={data} setOutput={setOutput} />
-                                : data.type === "2-select" ? <SelectNodes data={data} setOutput={setOutput} />
+                                : data.type === "1-select" ? <SelectNode1 data={data} setOutput={setOutput} />
+                                : data.type === "2-select" ? <SelectNode2 data={data} setOutput={setOutput} />
                                 : null
                             }
                         </div>
@@ -143,7 +168,7 @@ function Info({ data }) {
     return <p className={scss.info}>{data.info}</p>
 }
 
-function SelectNode({ data, setOutput }) {
+function SelectNode1({ data, setOutput }) {
 
 
     useEffect(() => {
@@ -165,8 +190,8 @@ function SelectNode({ data, setOutput }) {
     return (
         <div className={scss.select1}>
             {data.label && <label htmlFor="view-select">{data.label}</label>}
-            <select name="nodes-select" id="view-select" onChange={handleChange}>
-                {data.placeholderOption && <option value="" disabled selected>-</option>}
+            <select name="select" id="view-select" onChange={handleChange}>
+                <option value="" disabled selected>-</option>
                 {data.options.map((option, index) => <option key={index} value={option}>{option}</option>)}
             </select>
         </div>
@@ -174,13 +199,43 @@ function SelectNode({ data, setOutput }) {
 }
 
 
-function SelectNodes({ data, setOutput }) {
+function SelectNode2({ data, setOutput }) {
+
+
+    useEffect(() => {
+        // Run the setup function if it exists
+        if (data.setup) {
+            const result = data.setup()
+            setOutput(result)
+        }
+    }, [])
+
+    const handleChange = e => {
+        const selected = e.target.value
+        const selectId = e.target.name === "selectA" ? "A" : "B"
+        if (data.onChange) {
+            const result = data.onChange(selected, selectId)
+            setOutput(result)
+        }
+    }
+
     return (
-        <div>
-            <div>Nodes</div>
-            <div>Nodes</div>
-            <div>Nodes</div>
-            <div>Nodes</div>
+        <div className={scss.select2}>
+            <div className={scss.select_group}>
+                {data.labelA && <label htmlFor="view-selectA">{data.labelB}</label>}
+                <select name="selectA" id="view-selectA" onChange={handleChange} defaultValue="-">
+                    <option value="-" disabled>-</option>
+                    {data.optionsA.map((option, index) => <option key={index} value={option}>{option}</option>)}
+                </select>
+            </div>
+            { data.labelAB && <div className={scss.labelAB}>{data.labelAB}</div> }
+            <div className={scss.select_group}>
+                {data.labelB && <label htmlFor="view-selectB">{data.labelB}</label>}
+                <select name="selectB" id="view-selectB" onChange={handleChange} defaultValue="-">
+                    <option value="-" disabled>-</option>
+                    {data.optionsB.map((option, index) => <option key={index} value={option}>{option}</option>)}
+                </select>
+            </div>
         </div>
     )
 }
